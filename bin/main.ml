5f45096335ace89fd3347dc49993ba005fbe90d8
Thomas Leonard <talex5@gmail.com>
2020-12-22 16:12:50 +0000

    Update to latest opam-file-format
    Otherwise, we get a deprecation warning.

--- bin/main.ml
+++ bin/main.ml
@@ -65,7 +65,7 @@ let atom =
       let sversion = Re.Group.get sub 3 in
       let name = OpamPackage.Name.of_string sname in
       let sop = if sop = "." then "=" else sop in
-      let op = OpamLexer.relop sop in
+      let op = OpamLexer.FullPos.relop sop in
       let version = OpamPackage.Version.of_string sversion in
       `Ok (name, Some (op, version))
     with Not_found | Failure _ | OpamLexer.Error _ ->

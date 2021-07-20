dbea6d5c16fd9e42cfa9b531ca0515fca7aad716
Nathan Rebours <nathan.p.rebours@gmail.com>
2021-07-20 15:47:19 +0200

    Expose diagnostics rolemap in Solver

--- lib/solver.mli
+++ lib/solver.mli
@@ -1,1 +1,17 @@
-module Make (C : S.CONTEXT) : S.SOLVER with type t = C.t
+module Make (C : S.CONTEXT) : sig
+  include S.SOLVER with type t = C.t
+
+  module Input : sig
+    include Zeroinstall_solver.S.SOLVER_INPUT with type rejection = C.rejection
+  end
+
+  module Solver : sig
+    module Output : Zeroinstall_solver.S.SOLVER_RESULT with module Input = Input
+  end
+
+  module Diagnostics : sig
+    include module type of Zeroinstall_solver.Diagnostics(Solver.Output)
+  end
+
+  val diagnostics_rolemap : diagnostics -> Diagnostics.t
+end
